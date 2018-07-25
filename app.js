8e4653578100f21faac5069a14ed44fb2e8e9476
VatsalDevit <41566510+VatsalDevit@users.noreply.github.com>
2018-07-25 14:15:06 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+// Our handler function is passed a request and response object
+    app.get('/', function(req, res) {
+      // We must end the request when we are done handling it
+      res.write('Hello World!');
+      res.end();
+    });
+
+    restService.listen(process.env.PORT || 8000, function() {
+  console.log("Server up and listening");
+});
+
+    module.exports = app;
