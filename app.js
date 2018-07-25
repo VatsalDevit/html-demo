05c527a6e65ab26c9272d980667f4d98cbedbd71
VatsalDevit <41566510+VatsalDevit@users.noreply.github.com>
2018-07-25 19:11:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 // Our handler function is passed a request and response object
-    app.get('/', function(req, res) {
+    app.get("/create", function(req, res) {
       // We must end the request when we are done handling it
       res.write('Hello World!');
       res.end();
