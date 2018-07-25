c634a7df62450fbae57fe4caecdf1018ffb3a168
VatsalDevit <41566510+VatsalDevit@users.noreply.github.com>
2018-07-25 19:53:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,6 +27,20 @@ restService.post("/echo", function(req, res) {
   });
 });
 
+restService.post("/htmltest", function(req, res) {
+  var speech =
+    req.body.result &&
+    req.body.result.parameters &&
+    req.body.result.parameters.echoText
+      ? req.body.result.parameters.echoText
+      : "Seems like some problem. Speak again.Demo by Vatsal Patel";
+  return res.json({
+    speech: speech,
+    displayText: speech,
+    source: "Dialogflow-demo-sample"
+  });
+});
+
 restService.listen(process.env.PORT || 8000, function() {
   console.log("Server up and listening");
 });
