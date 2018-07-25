0a4e2aa831d7ef00d5f5f5f503cb6c1c7584a2fd
VatsalDevit <41566510+VatsalDevit@users.noreply.github.com>
2018-07-25 11:41:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,28 +30,14 @@ restService.post("/echo", function(req, res) {
   });
 });
 
-var server = http.createServer(function (req, resp) {
-    //3.
-    if (req.url === "/create") {
-        fs.readFile("AppPages/MyPage.html", function (error, pgResp) {
-            if (error) {
-                resp.writeHead(404);
-                resp.write('Contents you are looking are Not Found');
-            } else {
-                resp.writeHead(200, { 'Content-Type': 'text/html' });
-                resp.write(pgResp);
-            }
-             
-            resp.end();
-        });
-    } else {
-        //4.
+var server = http.createServer(function (req, resp) 
+    
+    if (req.url === "/create") {  
         resp.writeHead(200, { 'Content-Type': 'text/html' });
         resp.write('<h1>Product Manaager</h1><br /><br />To create product please enter: ' + req.url);
         resp.end();
-    }
+   
 });
-
 restService.listen(process.env.PORT || 8000, function() {
   console.log("Server up and listening");
 });
