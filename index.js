0ff087823ca95992740e46dec1b1fcb01fde4408
VatsalDevit <41566510+VatsalDevit@users.noreply.github.com>
2018-07-25 11:32:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+"use strict";
+
+var http = require('http');
+var fs = require('fs');
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const restService = express();
+
+restService.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+restService.use(bodyParser.json());
+
+restService.post("/echo", function(req, res) {
+  var speech =
+    req.body.result &&
+    req.body.result.parameters &&
+    req.body.result.parameters.echoText
+      ? req.body.result.parameters.echoText
+      : "Seems like some problem. Speak again.Demo by Vatsal Patel";
+  return res.json({
+    speech: speech,
+    displayText: speech,
+    source: "Dialogflow-demo-sample"
+  });
+});
+
+var server = http.createServer(function (req, resp) {
+    //3.
+    if (req.url === "/create") {
+        fs.readFile("AppPages/MyPage.html", function (error, pgResp) {
+            if (error) {
+                resp.writeHead(404);
+                resp.write('Contents you are looking are Not Found');
+            } else {
+                resp.writeHead(200, { 'Content-Type': 'text/html' });
+                resp.write(pgResp);
+            }
+             
+            resp.end();
+        });
+    } else {
+        //4.
+        resp.writeHead(200, { 'Content-Type': 'text/html' });
+        resp.write('<h1>Product Manaager</h1><br /><br />To create product please enter: ' + req.url);
+        resp.end();
+    }
+});
+
+restService.listen(process.env.PORT || 8000, function() {
+  console.log("Server up and listening");
+});
